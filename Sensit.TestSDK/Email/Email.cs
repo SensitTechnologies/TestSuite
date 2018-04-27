6127c2e25bb1fa0295d32d29a321a3fded75ce49
mclemcrew <michael.clemens@outlook.com>
2018-04-27 11:12:14 -0500

    Updated Database Design

--- Sensit.TestSDK/Email/Email.cs
+++ Sensit.TestSDK/Email/Email.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+using System.IO;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 
 namespace Sensit.TestSDK.Email
