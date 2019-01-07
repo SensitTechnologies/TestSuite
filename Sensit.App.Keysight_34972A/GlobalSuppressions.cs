2c07d893225ad6333641463076b0149369f4cf35
Adam Johnson <adamj537@gmail.com>
2019-01-07 15:21:12 -0600

    GlobalSuppressions.cs - added several of these files to suppress method name capitalization warnings for auto-generated methods.

---
+++ Sensit.App.Keysight_34972A/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Sensit.App.Keysight.FormKeysight.radioButton_CheckedChanged(System.Object,System.EventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Sensit.App.Keysight.FormKeysight.exitToolStripMenuItem_Click(System.Object,System.EventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Sensit.App.Keysight.FormKeysight.buttonFind_Click(System.Object,System.EventArgs)")]
+
