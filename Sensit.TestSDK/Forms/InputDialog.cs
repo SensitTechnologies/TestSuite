0a2aa94eb2a5e529d4863d34f2c8a88efb1c8ab4
Adam Johnson <adamj537@gmail.com>
2019-01-08 16:56:09 -0600

    Sensit.TestSDK.Forms.InputDialog - making the code and the form look nicer (not done yet).

--- Sensit.TestSDK/Forms/InputDialog.cs
+++ Sensit.TestSDK/Forms/InputDialog.cs
@@ -6,49 +6,69 @@ namespace Sensit.TestSDK.Forms
 {
 	public static class InputDialog
 	{
-		public static DialogResult Numeric(string caption, ref int input, int min = 0, int max = 100)
+		private static Form CreateBox(string caption)
 		{
 			// Create a new form.
-			Size size = new Size(200, 70);
+			Size size = new Size(150, 70);
+			// TODO:  Make the form autosize to the controls.
 			Form inputBox = new Form
 			{
 				// Set the form's properties.
+				MinimizeBox = false,
+				MaximizeBox = false,
 				FormBorderStyle = FormBorderStyle.FixedDialog,
 				ClientSize = size,
 				Text = caption
 			};
 
-			// Create a numeric up/down control.
-			NumericUpDown numericInput = new NumericUpDown();
-			numericInput.Size = new Size(size.Width - 10, 23);
-			numericInput.Location = new Point(5, 5);
-			numericInput.Value = input;
-			numericInput.Increment = 1;
-			numericInput.Minimum = min;
-			numericInput.Maximum = max;
-			inputBox.Controls.Add(numericInput);
-
 			// Create an OK button.
-			Button okButton = new Button();
-			okButton.DialogResult = DialogResult.OK;
-			okButton.Name = "buttonOK";
-			okButton.Size = new Size(75, 23);
-			okButton.Text = "&OK";
-			okButton.Location = new Point(size.Width - 80 - 80, 39);
+			Button okButton = new Button()
+			{
+				DialogResult = DialogResult.OK,
+				Name = "buttonOK",
+				Size = new Size(75, 23),
+				Text = "&OK",
+				Location = new Point(size.Width - 80 - 80, 39)
+			};
+
 			inputBox.Controls.Add(okButton);
 
 			// Create a Cancel button.
-			Button cancelButton = new Button();
-			cancelButton.DialogResult = DialogResult.Cancel;
-			cancelButton.Name = "buttonCancel";
-			cancelButton.Size = new Size(75, 23);
-			cancelButton.Text = "&Cancel";
-			cancelButton.Location = new Point(size.Width - 80, 39);
+			Button cancelButton = new Button()
+			{
+				DialogResult = DialogResult.Cancel,
+				Name = "buttonCancel",
+				Size = new Size(75, 23),
+				Text = "&Cancel",
+				Location = new Point(size.Width - 80, 39)
+			};
+
 			inputBox.Controls.Add(cancelButton);
 
 			// Set the DialogResult to be based on the buttons.
 			inputBox.AcceptButton = okButton;
 			inputBox.CancelButton = cancelButton;
+
+			return inputBox;
+		}
+
+		public static DialogResult Numeric(string caption, ref int input, int min = 0, int max = 100)
+		{
+			// Create a new form.
+			Form inputBox = CreateBox(caption);
+
+			// Create a numeric up/down control.
+			NumericUpDown numericInput = new NumericUpDown
+			{
+				Location = new Point(5, 5),
+				Dock = DockStyle.Fill,
+				Value = input,
+				Increment = 1,
+				Minimum = min,
+				Maximum = max
+			};
+			
+			inputBox.Controls.Add(numericInput);
 
 			// Show the form; read and return user input.
 			DialogResult result = inputBox.ShowDialog();
@@ -59,43 +79,13 @@ namespace Sensit.TestSDK.Forms
 		public static DialogResult Text(string caption, ref string input)
 		{
 			// Create a new form.
-			Size size = new Size(200, 70);
-			Form inputBox = new Form
-			{
-				// Set the form's properties.
-				FormBorderStyle = FormBorderStyle.FixedDialog,
-				ClientSize = size,
-				Text = caption
-			};
+			Form inputBox = CreateBox(caption);
 
 			// Create a numeric up/down control.
 			TextBox textBox = new TextBox();
-			textBox.Size = new Size(size.Width - 10, 23);
 			textBox.Location = new Point(5, 5);
 			textBox.Text = input;
 			inputBox.Controls.Add(textBox);
-
-			// Create an OK button.
-			Button okButton = new Button();
-			okButton.DialogResult = DialogResult.OK;
-			okButton.Name = "buttonOK";
-			okButton.Size = new Size(75, 23);
-			okButton.Text = "&OK";
-			okButton.Location = new Point(size.Width - 80 - 80, 39);
-			inputBox.Controls.Add(okButton);
-
-			// Create a Cancel button.
-			Button cancelButton = new Button();
-			cancelButton.DialogResult = DialogResult.Cancel;
-			cancelButton.Name = "buttonCancel";
-			cancelButton.Size = new Size(75, 23);
-			cancelButton.Text = "&Cancel";
-			cancelButton.Location = new Point(size.Width - 80, 39);
-			inputBox.Controls.Add(cancelButton);
-
-			// Set the DialogResult to be based on the buttons.
-			inputBox.AcceptButton = okButton;
-			inputBox.CancelButton = cancelButton;
 
 			// Show the form; read and return user input.
 			DialogResult result = inputBox.ShowDialog();
