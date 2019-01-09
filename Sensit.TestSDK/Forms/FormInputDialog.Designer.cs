b30f85ad02a7e240d7333f00958b7d8196118fec
Adam Johnson <adamj537@gmail.com>
2019-01-09 12:23:10 -0600

    Added new FormInputDialog, but it's not done yet, and will be very low priority.

---
+++ Sensit.TestSDK/Forms/FormInputDialog.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Sensit.TestSDK.Forms
+{
+	partial class FormInputDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.buttonOk = new System.Windows.Forms.Button();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.AutoSize = true;
+			this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.buttonOk, 0, 3);
+			this.tableLayoutPanel1.Controls.Add(this.buttonCancel, 1, 3);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 4;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(162, 78);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// buttonOk
+			// 
+			this.buttonOk.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.buttonOk.Location = new System.Drawing.Point(3, 52);
+			this.buttonOk.Name = "buttonOk";
+			this.buttonOk.Size = new System.Drawing.Size(75, 23);
+			this.buttonOk.TabIndex = 0;
+			this.buttonOk.Text = "&OK";
+			this.buttonOk.UseVisualStyleBackColor = true;
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.buttonCancel.Location = new System.Drawing.Point(84, 52);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+			this.buttonCancel.TabIndex = 1;
+			this.buttonCancel.Text = "&Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.tableLayoutPanel1.SetColumnSpan(this.numericUpDown1, 2);
+			this.numericUpDown1.Location = new System.Drawing.Point(21, 16);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label1.AutoSize = true;
+			this.tableLayoutPanel1.SetColumnSpan(this.label1, 2);
+			this.label1.Location = new System.Drawing.Point(81, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 13);
+			this.label1.TabIndex = 3;
+			// 
+			// FormInputDialog
+			// 
+			this.AcceptButton = this.buttonOk;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.CancelButton = this.buttonCancel;
+			this.ClientSize = new System.Drawing.Size(186, 95);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FormInputDialog";
+			this.Text = "Input Dialog";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button buttonOk;
+		private System.Windows.Forms.Button buttonCancel;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ Sensit.TestSDK/Forms/FormInputDialog.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Forms;
+
+namespace Sensit.TestSDK.Forms
+{
+	// This class is an attempt to make the "InputDialog.cs" look nicer by using the form designer.
+	// It was set aside because it wasn't worth the time commitment at the time.
+	// TODO:  Implement this class as a messagebox which returns a dialog result.
+	// TODO:  Use this class instead of the "InputDialog.cs" class in the SDK.
+	public partial class FormInputDialog : Form
+	{
+		public FormInputDialog()
+		{
+			InitializeComponent();
+			FormBorderStyle = FormBorderStyle.FixedDialog;
+		}
+
+		public FormInputDialog(string caption)
+		{
+			// Set the form's caption.
+			Text = caption;
+			FormBorderStyle = FormBorderStyle.FixedDialog;
+		}
+
+		public FormInputDialog(string caption, string label)
+		{
+			Text = caption;
+			Label = label;
+			FormBorderStyle = FormBorderStyle.FixedDialog;
+		}
+
+		/// <summary>
+		/// Text displayed to the user above the input control.
+		/// </summary>
+		/// <remarks>
+		/// Blank by default.
+		/// </remarks>
+		public string Label
+		{
+			get => label1.Text;
+			set => label1.Text = value;
+		}
+
+
+	}
+}
