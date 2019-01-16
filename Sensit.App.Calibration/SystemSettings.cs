e2bd596153d6a7d7fa7001849c8b4869aacb55a7
Adam Johnson <adamj537@gmail.com>
2019-01-16 16:11:35 -0600

    Capturing file's original contents.

---
+++ Sensit.App.Calibration/SystemSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Sensit.App.Calibration
+{
+	[Serializable]
+	public class SystemSettings
+	{
+		[Category("Hardware"), Description("Serial Port")]
+		public string MassFlowController { get; set; } = "";
+	}
+}
