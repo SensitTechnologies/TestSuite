7429aa81110347eb33a4492638a623ffa8a91953
Adam Johnson <adamj537@gmail.com>
2019-01-16 17:03:04 -0600

    Updated assembly info (which appears in About Box).

--- Sensit.App.Calibration/Properties/AssemblyInfo.cs
+++ Sensit.App.Calibration/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sensit.App.Calibration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sensor testing and calibration application.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sensit.App.Calibration")]
+[assembly: AssemblyCompany("Sensit Technologies")]
+[assembly: AssemblyProduct("Calibration App")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
