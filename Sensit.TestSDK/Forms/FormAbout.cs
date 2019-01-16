407b8d8344aba31dd18dfd3998fe8b2d9d88a0a4
Adam Johnson <adamj537@gmail.com>
2019-01-16 16:02:56 -0600

    Added generic about form. It's almost the same as an auto-generated one, but not quite (details in comments).

---
+++ Sensit.TestSDK/Forms/FormAbout.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Sensit.TestSDK.Forms
+{
+	/// <summary>
+	/// Generic About Box that can be used in any application.
+	/// </summary>
+	/// <remarks>
+	/// This form is the same as the stock "AboutBox" form, which you get from
+	/// Right click --> Add --> New Item --> About Box, except that
+	/// "GetExecutingAssembly" has been replaced with "GetEntryAssembly" in
+	/// the Asembly Attribute Accessors.
+	/// </remarks>
+	public partial class FormAbout : Form
+	{
+		public FormAbout()
+		{
+			InitializeComponent();
+
+			this.Text = String.Format("About {0}", AssemblyTitle);
+			this.labelProductName.Text = AssemblyProduct;
+			this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+			this.labelCopyright.Text = AssemblyCopyright;
+			this.labelCompanyName.Text = AssemblyCompany;
+			this.textBoxDescription.Text = AssemblyDescription;
+		}
+
+		#region Assembly Attribute Accessors
+
+		public string AssemblyTitle
+		{
+			get
+			{
+				object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+				if (attributes.Length > 0)
+				{
+					AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+					if (titleAttribute.Title != "")
+					{
+						return titleAttribute.Title;
+					}
+				}
+				return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().CodeBase);
+			}
+		}
+
+		public string AssemblyVersion
+		{
+			get
+			{
+				return Assembly.GetEntryAssembly().GetName().Version.ToString();
+			}
+		}
+
+		public string AssemblyDescription
+		{
+			get
+			{
+				object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+			}
+		}
+
+		public string AssemblyProduct
+		{
+			get
+			{
+				object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyProductAttribute)attributes[0]).Product;
+			}
+		}
+
+		public string AssemblyCopyright
+		{
+			get
+			{
+				object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+			}
+		}
+
+		public string AssemblyCompany
+		{
+			get
+			{
+				object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+				if (attributes.Length == 0)
+				{
+					return "";
+				}
+				return ((AssemblyCompanyAttribute)attributes[0]).Company;
+			}
+		}
+		#endregion
+	}
+}
