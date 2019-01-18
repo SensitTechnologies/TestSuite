83164c862a7a872738d22c575792e29e25a863b6
Adam Johnson <adamj537@gmail.com>
2019-01-18 14:00:18 -0600

    Added user setting "NumDuts".

--- Sensit.App.Calibration/Properties/Settings.Designer.cs
+++ Sensit.App.Calibration/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace Sensit.App.Calibration.Properties {
                 this["Test"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int NumDuts {
+            get {
+                return ((int)(this["NumDuts"]));
+            }
+            set {
+                this["NumDuts"] = value;
+            }
+        }
     }
 }
