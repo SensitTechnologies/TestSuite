5e3bb71585a2992fa65d56969be2481bdbc8a9bc
Adam Johnson <adamj537@gmail.com>
2019-01-30 02:09:25 -0600

    UserCustomStep - replaced methods with properties; added groupbox for organization.  This control isn't used in any app right now, but I had time for some cleanup.

--- Sensit.TestSDK/Controls/UserCustomStep.Designer.cs
+++ Sensit.TestSDK/Controls/UserCustomStep.Designer.cs
@@ -28,100 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.failureExplanation = new System.Windows.Forms.TextBox();
-            this.radioButtonPass = new System.Windows.Forms.RadioButton();
-            this.radioButtonFail = new System.Windows.Forms.RadioButton();
-            this.expectedResult = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AllowDrop = true;
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(2, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 70);
-            this.label1.MaximumSize = new System.Drawing.Size(262, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Step";
-            // 
-            // failureExplanation
-            // 
-            this.failureExplanation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.failureExplanation.BackColor = System.Drawing.Color.White;
-            this.failureExplanation.Location = new System.Drawing.Point(5, 110);
-            this.failureExplanation.Margin = new System.Windows.Forms.Padding(0);
-            this.failureExplanation.Multiline = true;
-            this.failureExplanation.Name = "failureExplanation";
-            this.failureExplanation.Size = new System.Drawing.Size(259, 39);
-            this.failureExplanation.TabIndex = 3;
-            this.failureExplanation.Visible = false;
-            // 
-            // radioButtonPass
-            // 
-            this.radioButtonPass.AutoSize = true;
-            this.radioButtonPass.Location = new System.Drawing.Point(292, 2);
-            this.radioButtonPass.Margin = new System.Windows.Forms.Padding(2);
-            this.radioButtonPass.Name = "radioButtonPass";
-            this.radioButtonPass.Size = new System.Drawing.Size(48, 17);
-            this.radioButtonPass.TabIndex = 4;
-            this.radioButtonPass.TabStop = true;
-            this.radioButtonPass.Text = "Pass";
-            this.radioButtonPass.UseVisualStyleBackColor = true;
-            this.radioButtonPass.CheckedChanged += new System.EventHandler(this.radioButtonPass_CheckedChanged);
-            // 
-            // radioButtonFail
-            // 
-            this.radioButtonFail.AutoSize = true;
-            this.radioButtonFail.Location = new System.Drawing.Point(348, 2);
-            this.radioButtonFail.Margin = new System.Windows.Forms.Padding(2);
-            this.radioButtonFail.Name = "radioButtonFail";
-            this.radioButtonFail.Size = new System.Drawing.Size(41, 17);
-            this.radioButtonFail.TabIndex = 5;
-            this.radioButtonFail.TabStop = true;
-            this.radioButtonFail.Text = "Fail";
-            this.radioButtonFail.UseVisualStyleBackColor = true;
-            this.radioButtonFail.CheckedChanged += new System.EventHandler(this.radioButtonFail_CheckedChanged);
-            // 
-            // expectedResult
-            // 
-            this.expectedResult.AllowDrop = true;
-            this.expectedResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.expectedResult.AutoSize = true;
-            this.expectedResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.6F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.expectedResult.Location = new System.Drawing.Point(3, 81);
-            this.expectedResult.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
-            this.expectedResult.MaximumSize = new System.Drawing.Size(262, 0);
-            this.expectedResult.Name = "expectedResult";
-            this.expectedResult.Size = new System.Drawing.Size(73, 12);
-            this.expectedResult.TabIndex = 6;
-            this.expectedResult.Text = "Expected Result";
-            // 
-            // UserCustomStep
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.Controls.Add(this.expectedResult);
-            this.Controls.Add(this.radioButtonFail);
-            this.Controls.Add(this.radioButtonPass);
-            this.Controls.Add(this.failureExplanation);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 7, 2, 20);
-            this.Name = "UserCustomStep";
-            this.Size = new System.Drawing.Size(398, 150);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.textBoxActualResult = new System.Windows.Forms.TextBox();
+			this.radioButtonPass = new System.Windows.Forms.RadioButton();
+			this.radioButtonFail = new System.Windows.Forms.RadioButton();
+			this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.labelExpectedResult = new System.Windows.Forms.Label();
+			this.labelInstructions = new System.Windows.Forms.Label();
+			this.groupBoxStep = new System.Windows.Forms.GroupBox();
+			this.tableLayoutPanel.SuspendLayout();
+			this.groupBoxStep.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBoxActualResult
+			// 
+			this.textBoxActualResult.BackColor = System.Drawing.Color.White;
+			this.textBoxActualResult.Location = new System.Drawing.Point(0, 42);
+			this.textBoxActualResult.Margin = new System.Windows.Forms.Padding(0);
+			this.textBoxActualResult.Multiline = true;
+			this.textBoxActualResult.Name = "textBoxActualResult";
+			this.textBoxActualResult.Size = new System.Drawing.Size(200, 32);
+			this.textBoxActualResult.TabIndex = 3;
+			this.textBoxActualResult.Visible = false;
+			// 
+			// radioButtonPass
+			// 
+			this.radioButtonPass.AutoSize = true;
+			this.radioButtonPass.Location = new System.Drawing.Point(202, 2);
+			this.radioButtonPass.Margin = new System.Windows.Forms.Padding(2);
+			this.radioButtonPass.Name = "radioButtonPass";
+			this.radioButtonPass.Size = new System.Drawing.Size(48, 17);
+			this.radioButtonPass.TabIndex = 4;
+			this.radioButtonPass.TabStop = true;
+			this.radioButtonPass.Text = "Pass";
+			this.radioButtonPass.UseVisualStyleBackColor = true;
+			this.radioButtonPass.CheckedChanged += new System.EventHandler(this.radioButtonPass_CheckedChanged);
+			// 
+			// radioButtonFail
+			// 
+			this.radioButtonFail.AutoSize = true;
+			this.radioButtonFail.Location = new System.Drawing.Point(202, 23);
+			this.radioButtonFail.Margin = new System.Windows.Forms.Padding(2);
+			this.radioButtonFail.Name = "radioButtonFail";
+			this.radioButtonFail.Size = new System.Drawing.Size(41, 17);
+			this.radioButtonFail.TabIndex = 5;
+			this.radioButtonFail.TabStop = true;
+			this.radioButtonFail.Text = "Fail";
+			this.radioButtonFail.UseVisualStyleBackColor = true;
+			this.radioButtonFail.CheckedChanged += new System.EventHandler(this.radioButtonFail_CheckedChanged);
+			// 
+			// tableLayoutPanel
+			// 
+			this.tableLayoutPanel.AutoSize = true;
+			this.tableLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel.ColumnCount = 2;
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel.Controls.Add(this.labelExpectedResult, 0, 1);
+			this.tableLayoutPanel.Controls.Add(this.radioButtonPass, 1, 0);
+			this.tableLayoutPanel.Controls.Add(this.labelInstructions, 0, 0);
+			this.tableLayoutPanel.Controls.Add(this.textBoxActualResult, 0, 2);
+			this.tableLayoutPanel.Controls.Add(this.radioButtonFail, 1, 1);
+			this.tableLayoutPanel.Location = new System.Drawing.Point(6, 19);
+			this.tableLayoutPanel.Name = "tableLayoutPanel";
+			this.tableLayoutPanel.RowCount = 3;
+			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel.Size = new System.Drawing.Size(252, 74);
+			this.tableLayoutPanel.TabIndex = 7;
+			// 
+			// labelExpectedResult
+			// 
+			this.labelExpectedResult.AutoSize = true;
+			this.labelExpectedResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelExpectedResult.Location = new System.Drawing.Point(3, 21);
+			this.labelExpectedResult.Name = "labelExpectedResult";
+			this.labelExpectedResult.Size = new System.Drawing.Size(85, 13);
+			this.labelExpectedResult.TabIndex = 9;
+			this.labelExpectedResult.Text = "Expected Result";
+			// 
+			// labelInstructions
+			// 
+			this.labelInstructions.AutoSize = true;
+			this.labelInstructions.Location = new System.Drawing.Point(3, 0);
+			this.labelInstructions.Name = "labelInstructions";
+			this.labelInstructions.Size = new System.Drawing.Size(61, 13);
+			this.labelInstructions.TabIndex = 7;
+			this.labelInstructions.Text = "Instructions";
+			// 
+			// groupBoxStep
+			// 
+			this.groupBoxStep.AutoSize = true;
+			this.groupBoxStep.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.groupBoxStep.Controls.Add(this.tableLayoutPanel);
+			this.groupBoxStep.Location = new System.Drawing.Point(4, 4);
+			this.groupBoxStep.Name = "groupBoxStep";
+			this.groupBoxStep.Size = new System.Drawing.Size(264, 112);
+			this.groupBoxStep.TabIndex = 8;
+			this.groupBoxStep.TabStop = false;
+			this.groupBoxStep.Text = "Step";
+			// 
+			// UserCustomStep
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.Controls.Add(this.groupBoxStep);
+			this.Margin = new System.Windows.Forms.Padding(2, 7, 2, 20);
+			this.Name = "UserCustomStep";
+			this.Size = new System.Drawing.Size(271, 119);
+			this.tableLayoutPanel.ResumeLayout(false);
+			this.tableLayoutPanel.PerformLayout();
+			this.groupBoxStep.ResumeLayout(false);
+			this.groupBoxStep.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox failureExplanation;
+        private System.Windows.Forms.TextBox textBoxActualResult;
         private System.Windows.Forms.RadioButton radioButtonPass;
         private System.Windows.Forms.RadioButton radioButtonFail;
-        private System.Windows.Forms.Label expectedResult;
-    }
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+		private System.Windows.Forms.Label labelInstructions;
+		private System.Windows.Forms.GroupBox groupBoxStep;
+		private System.Windows.Forms.Label labelExpectedResult;
+	}
 }
