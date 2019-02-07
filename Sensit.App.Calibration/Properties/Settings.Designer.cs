4600d95cb51b651a28bd9eabd385871e11fb9986
Adam Johnson <adamj537@gmail.com>
2019-02-07 17:12:37 -0600

    Changed default values in GUI.

--- Sensit.App.Calibration/Properties/Settings.Designer.cs
+++ Sensit.App.Calibration/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Sensit.App.Calibration.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Simulator")]
         public string Model {
             get {
                 return ((string)(this["Model"]));
@@ -49,7 +49,7 @@ namespace Sensit.App.Calibration.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Linearity")]
         public string Test {
             get {
                 return ((string)(this["Test"]));
