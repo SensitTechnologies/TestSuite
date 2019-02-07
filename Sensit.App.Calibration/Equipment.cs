c28639620226e1b67e0ccbd43dfb6fdafa7ee2a4
Adam Johnson <adamj537@gmail.com>
2019-02-07 14:08:08 -0600

    Sensit.App.Calibration/Equipment - comments in preparation for making the equipment respond to user settings for what to use.

--- Sensit.App.Calibration/Equipment.cs
+++ Sensit.App.Calibration/Equipment.cs
@@ -4,17 +4,29 @@ using Sensit.TestSDK.Settings;
 
 namespace Sensit.App.Calibration
 {
+	/// <summary>
+	/// Manage devices used to collect data during tests.
+	/// </summary>
+	/// <remarks>
+	/// We have one of each possible type of device.  Depending on the user's
+	/// selections, we choose which device(s) to use and pass to the user for
+	/// each needed interface.
+	/// 
+	/// DUT interface devices are a different sort of beast that may make more
+	/// sense to manage elsewhere, but they're here for now.
+	/// </remarks>
 	public class Equipment
 	{
 		// system settings
 		private readonly EquipmentSettings _settings;
 
+		// generic manual device, used whenever the user selects "Manual" option for equipment.
 		Manual _manual;
 
 		// mass flow controller for analyte gas
 		private ColeParmerMFC _mfcAnalyte;
 
-		// mass flow controller for diluent gas
+		// mass flow controller for diluent gas (needed for gas mixing)
 		private ColeParmerMFC _mfcDiluent;
 
 		// both mass flow controllers combined for mixing gas
@@ -31,15 +43,18 @@ namespace Sensit.App.Calibration
 			// Read system settings.
 			_settings = Settings.Load<EquipmentSettings>(Properties.Settings.Default.SystemSettingsFile);
 
-			// TODO:  (Low priority) Create test equipment objects (as specified in settings).
+			// Create test equipment objects.
+			// Only the ones chosen by the user will end up being used.
 			_mfcAnalyte = new ColeParmerMFC();
 			_mfcDiluent = new ColeParmerMFC();
 			_gasMixer = new GasConcentrationDevice(_mfcDiluent, _mfcDiluent, _mfcAnalyte, _mfcAnalyte);
 			_datalogger = new Keysight_34972A();
-
 			_manual = new Manual();
 		}
 
+		/// <summary>
+		///  DUT interface device used to take readings from sensors/products/etc.
+		/// </summary>
 		public IDutInterfaceDevice DutInterface => _datalogger;
 
 		public IGasConcentrationController GasController => _manual;
@@ -56,7 +71,7 @@ namespace Sensit.App.Calibration
 			_mfcDiluent.Open(_settings?.DiluentControllerPort);
 
 			// Configure the datalogger.
-			//_datalogger.Open();
+			_datalogger.Open();
 		}
 
 		/// <summary>
