7dcc95fa6c0bf1b6889dfd119b5a7694e770d1b7
Adam Johnson <adamj537@gmail.com>
2019-02-15 12:04:31 -0600

    Corrected Category attributes.

--- Sensit.App.Calibration/EquipmentSettings.cs
+++ Sensit.App.Calibration/EquipmentSettings.cs
@@ -8,45 +8,45 @@ namespace Sensit.App.Calibration
 	[Serializable]
 	public class ReferenceSetting
 	{
-		[Category("Device Type"), Description("Variable that device measures.")]
+		[Category("Reference Setting"), Description("Variable that device measures.")]
 		public Test.VariableType DeviceType { get; set; }
 
-		[Category("Serial Ports"), Description("Serial ports used by device.")]
+		[Category("Reference Setting"), Description("Serial ports used by device.")]
 		public List<string> SerialPorts { get; set; }
 
-		[Category("Gas Selection"), Description("Gas measured by device.")]
+		[Category("Reference Setting"), Description("Gas measured by device.")]
 		public List<Gas> GasSelection { get; set; }
 	}
 
 	[Serializable]
 	public class ControlSetting
 	{
-		[Category("Device Type"), Description("Interface used by control device.")]
+		[Category("Control Setting"), Description("Interface used by control device.")]
 		public Test.VariableType DeviceType { get; set; }
 
-		[Category("Serial Ports"), Description("Serial ports used by device.")]
+		[Category("Control Setting"), Description("Serial ports used by device.")]
 		public List<string> SerialPorts { get; set; }
 
-		[Category("Gas"), Description("Gas controlled by device.")]
+		[Category("Control Setting"), Description("Gas controlled by device.")]
 		public List<Gas> GasSelection { get; set; }
 
-		[Category("Sub-device"), Description("Devices used to form a composite device (i.e. gas mixer using two mass flow controllers).")]
+		[Category("Control Setting"), Description("Devices used to form a composite device (i.e. gas mixer using two mass flow controllers).")]
 		public List<ReferenceSetting> SubDevice { get; set; }
 	}
 
 	[Serializable]
 	public class EquipmentSettings : Attribute
 	{
-		[Category("Control Device Settings"), Description("Settings for control devices.")]
+		[Category("Equipment Settings"), Description("Settings for control devices.")]
 		public List<ControlSetting> ControlSettings { get; set; }
 
-		[Category("Reference Device Settings"), Description("Settings for reference devices.")]
+		[Category("Equipment Settings"), Description("Settings for reference devices.")]
 		public List<ReferenceSetting> ReferenceSettings { get; set; }
 
-		[Category("Analyte Mass Flow Controller"), Description("Serial Port")]
+		[Category("Equipment Settings"), Description("Serial Port for Analyte Mass Flow Controller")]
 		public string AnalyteControllerPort { get; set; } = "COM7";
 
-		[Category("Diluent Mass Flow Controller"), Description("Serial Port")]
+		[Category("Equipment Settings"), Description("Serial Port for Diluent Mass Flow Controller")]
 		public string DiluentControllerPort { get; set; } = "COM6";
 	}
 }
