4913ab61f5c6ae5eff56cab8956e4dd4737134fd
Adam Johnson <adamj537@gmail.com>
2019-02-20 18:08:09 -0600

    Added test exception class.

---
+++ Sensit.TestSDK/Exceptions/TestExceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sensit.TestSDK.Exceptions
+{
+	/// <summary>
+	/// Unrecoverable test error.
+	/// </summary>
+	public class TestException : Exception
+	{
+		public TestException() { }
+		public TestException(string message) : base(message) { }
+		public TestException(string message, Exception inner) : base(message, inner) { }
+	}
+}
