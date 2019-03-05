1c4f99bcab9e8e47d98b7258c4b05b5abb1f3469
Adam Johnson <adamj537@gmail.com>
2019-03-05 17:06:58 -0600

    BUG FIX:  Controllers and References were not instantiated.

--- Sensit.App.Calibration/Equipment.cs
+++ Sensit.App.Calibration/Equipment.cs
@@ -61,8 +61,11 @@ namespace Sensit.App.Calibration
 			_datalogger = new Keysight_34972A();
 			_manual = new Manual();
 
+			Controllers = new Dictionary<VariableType, IControlDevice>();
 			Controllers.Add(VariableType.GasConcentration, _gasMixer);
 			Controllers.Add(VariableType.MassFlow, _gasMixer);
+
+			References = new Dictionary<VariableType, IReferenceDevice>();
 			References.Add(VariableType.GasConcentration, _gasMixer);
 			References.Add(VariableType.MassFlow, _gasMixer);
 		}
