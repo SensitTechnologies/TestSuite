3650b6acc392c0d6ef0695d4d3c0c81afb13503b
Adam Johnson <adamj537@gmail.com>
2019-11-14 17:28:15 -0600

    Adding a method to generate a alphabetic code for a year, as part of possible future serial number generation.

---
+++ Sensit.TestSDK/Calculations/SerialNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using Sensit.TestSDK.Exceptions;
+
+namespace Sensit.TestSDK.Calculations
+{
+	public static class SerialNumber
+	{
+		private static readonly uint BASE_YEAR = 2019;
+		private static readonly uint MAX_YEAR = BASE_YEAR + ((26 * 26) - 1);
+
+		public static string YearCode(uint year)
+		{
+			if ((year < BASE_YEAR) || (year > MAX_YEAR))
+			{
+				throw new TestException("Invalid year " + year.ToString());
+			}
+
+			// Base 26 representation:  A = 0, Z = 25
+			uint offset0 = (year - BASE_YEAR) / 26;
+			uint offset1 = (year - BASE_YEAR) % 26;
+
+			StringBuilder code = new StringBuilder();
+			if (offset1 > 0)
+			{
+				code.Append((char)('A' + offset1 - 1));
+			}
+
+			code.Append((char)('A' + offset0));
+
+			return code.ToString();
+		}
+	}
+}
