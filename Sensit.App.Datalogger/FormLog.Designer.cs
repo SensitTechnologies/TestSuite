6182d471827bebb047b490fe54db95b6c8212292
Adam Johnson <adamj537@gmail.com>
2020-07-08 20:59:39 -0500

    Datalogging app (in progress!).

---
+++ Sensit.App.Datalogger/FormLog.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace Sensit.App.Datalogger
+{
+	partial class FormLog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.groupBoxMassFlow = new System.Windows.Forms.GroupBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.textBoxFilename = new System.Windows.Forms.TextBox();
+			this.buttonBrowse = new System.Windows.Forms.Button();
+			this.groupBoxSerialPort = new System.Windows.Forms.GroupBox();
+			this.comboBoxSerialPort = new System.Windows.Forms.ComboBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.numericUpDownInterval = new System.Windows.Forms.NumericUpDown();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.textBoxResponse = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.textBoxCommand = new System.Windows.Forms.TextBox();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.buttonStop = new System.Windows.Forms.Button();
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.menuStrip1.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
+			this.groupBoxMassFlow.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.groupBoxSerialPort.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).BeginInit();
+			this.groupBox3.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			this.tableLayoutPanel3.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(288, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+			this.exitToolStripMenuItem.Text = "&Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 341);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(288, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+			this.toolStripStatusLabel1.Text = "Ready";
+			// 
+			// groupBoxMassFlow
+			// 
+			this.groupBoxMassFlow.AutoSize = true;
+			this.groupBoxMassFlow.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.groupBoxMassFlow.Controls.Add(this.tableLayoutPanel1);
+			this.groupBoxMassFlow.Dock = System.Windows.Forms.DockStyle.Top;
+			this.groupBoxMassFlow.Location = new System.Drawing.Point(0, 24);
+			this.groupBoxMassFlow.Name = "groupBoxMassFlow";
+			this.groupBoxMassFlow.Size = new System.Drawing.Size(288, 67);
+			this.groupBoxMassFlow.TabIndex = 7;
+			this.groupBoxMassFlow.TabStop = false;
+			this.groupBoxMassFlow.Text = "Filename";
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.AutoSize = true;
+			this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Controls.Add(this.textBoxFilename, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.buttonBrowse, 1, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 19);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(275, 29);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// textBoxFilename
+			// 
+			this.textBoxFilename.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.textBoxFilename.Location = new System.Drawing.Point(3, 4);
+			this.textBoxFilename.Name = "textBoxFilename";
+			this.textBoxFilename.Size = new System.Drawing.Size(188, 20);
+			this.textBoxFilename.TabIndex = 9;
+			// 
+			// buttonBrowse
+			// 
+			this.buttonBrowse.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.buttonBrowse.Location = new System.Drawing.Point(197, 3);
+			this.buttonBrowse.Name = "buttonBrowse";
+			this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+			this.buttonBrowse.TabIndex = 2;
+			this.buttonBrowse.Text = "Browse";
+			this.buttonBrowse.UseVisualStyleBackColor = true;
+			this.buttonBrowse.Click += new System.EventHandler(this.ButtonBrowse_Click);
+			// 
+			// groupBoxSerialPort
+			// 
+			this.groupBoxSerialPort.AutoSize = true;
+			this.groupBoxSerialPort.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.groupBoxSerialPort.Controls.Add(this.comboBoxSerialPort);
+			this.groupBoxSerialPort.Location = new System.Drawing.Point(0, 97);
+			this.groupBoxSerialPort.Name = "groupBoxSerialPort";
+			this.groupBoxSerialPort.Size = new System.Drawing.Size(140, 60);
+			this.groupBoxSerialPort.TabIndex = 8;
+			this.groupBoxSerialPort.TabStop = false;
+			this.groupBoxSerialPort.Text = "Serial Port";
+			// 
+			// comboBoxSerialPort
+			// 
+			this.comboBoxSerialPort.FormattingEnabled = true;
+			this.comboBoxSerialPort.Location = new System.Drawing.Point(13, 20);
+			this.comboBoxSerialPort.Name = "comboBoxSerialPort";
+			this.comboBoxSerialPort.Size = new System.Drawing.Size(121, 21);
+			this.comboBoxSerialPort.TabIndex = 0;
+			this.comboBoxSerialPort.SelectedIndexChanged += new System.EventHandler(this.ComboBoxSerialPort_SelectedIndexChanged);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.AutoSize = true;
+			this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.groupBox1.Controls.Add(this.numericUpDownInterval);
+			this.groupBox1.Location = new System.Drawing.Point(146, 97);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(143, 60);
+			this.groupBox1.TabIndex = 10;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Sample Interval (ms)";
+			// 
+			// numericUpDownInterval
+			// 
+			this.numericUpDownInterval.Location = new System.Drawing.Point(14, 21);
+			this.numericUpDownInterval.Maximum = new decimal(new int[] {
+            36000000,
+            0,
+            0,
+            0});
+			this.numericUpDownInterval.Name = "numericUpDownInterval";
+			this.numericUpDownInterval.Size = new System.Drawing.Size(123, 20);
+			this.numericUpDownInterval.TabIndex = 1;
+			this.numericUpDownInterval.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.numericUpDownInterval.ValueChanged += new System.EventHandler(this.NumericUpDownInterval_ValueChanged);
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.AutoSize = true;
+			this.groupBox3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.groupBox3.Controls.Add(this.textBoxResponse);
+			this.groupBox3.Location = new System.Drawing.Point(0, 227);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(286, 58);
+			this.groupBox3.TabIndex = 11;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Response";
+			// 
+			// textBoxResponse
+			// 
+			this.textBoxResponse.Location = new System.Drawing.Point(11, 19);
+			this.textBoxResponse.Name = "textBoxResponse";
+			this.textBoxResponse.Size = new System.Drawing.Size(269, 20);
+			this.textBoxResponse.TabIndex = 10;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.AutoSize = true;
+			this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.groupBox2.Controls.Add(this.textBoxCommand);
+			this.groupBox2.Location = new System.Drawing.Point(0, 163);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(286, 58);
+			this.groupBox2.TabIndex = 12;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Command";
+			// 
+			// textBoxCommand
+			// 
+			this.textBoxCommand.Location = new System.Drawing.Point(11, 19);
+			this.textBoxCommand.Name = "textBoxCommand";
+			this.textBoxCommand.Size = new System.Drawing.Size(269, 20);
+			this.textBoxCommand.TabIndex = 10;
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.AutoSize = true;
+			this.groupBox4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.groupBox4.Controls.Add(this.tableLayoutPanel3);
+			this.groupBox4.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.groupBox4.Location = new System.Drawing.Point(0, 287);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(288, 54);
+			this.groupBox4.TabIndex = 13;
+			this.groupBox4.TabStop = false;
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.AutoSize = true;
+			this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel3.ColumnCount = 1;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel2, 0, 0);
+			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 16);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 1;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(282, 35);
+			this.tableLayoutPanel3.TabIndex = 1;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.tableLayoutPanel2.AutoSize = true;
+			this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel2.ColumnCount = 2;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Controls.Add(this.buttonStop, 1, 0);
+			this.tableLayoutPanel2.Controls.Add(this.buttonStart, 0, 0);
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(60, 3);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(162, 29);
+			this.tableLayoutPanel2.TabIndex = 0;
+			// 
+			// buttonStop
+			// 
+			this.buttonStop.Location = new System.Drawing.Point(84, 3);
+			this.buttonStop.Name = "buttonStop";
+			this.buttonStop.Size = new System.Drawing.Size(75, 23);
+			this.buttonStop.TabIndex = 1;
+			this.buttonStop.Text = "Stop";
+			this.buttonStop.UseVisualStyleBackColor = true;
+			this.buttonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Location = new System.Drawing.Point(3, 3);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(75, 23);
+			this.buttonStart.TabIndex = 0;
+			this.buttonStart.Text = "Start";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+			// 
+			// FormLog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(288, 363);
+			this.Controls.Add(this.groupBox4);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.groupBoxSerialPort);
+			this.Controls.Add(this.groupBoxMassFlow);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "FormLog";
+			this.Text = "Data logger";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormLog_FormClosing);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.groupBoxMassFlow.ResumeLayout(false);
+			this.groupBoxMassFlow.PerformLayout();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.groupBoxSerialPort.ResumeLayout(false);
+			this.groupBox1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).EndInit();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
+			this.tableLayoutPanel3.ResumeLayout(false);
+			this.tableLayoutPanel3.PerformLayout();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.GroupBox groupBoxMassFlow;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TextBox textBoxFilename;
+		private System.Windows.Forms.Button buttonBrowse;
+		private System.Windows.Forms.GroupBox groupBoxSerialPort;
+		private System.Windows.Forms.ComboBox comboBoxSerialPort;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.TextBox textBoxResponse;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.TextBox textBoxCommand;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.Button buttonStop;
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.NumericUpDown numericUpDownInterval;
+	}
+}
+
---
+++ Sensit.App.Datalogger/FormLog.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+using Sensit.TestSDK.Files;
+
+namespace Sensit.App.Datalogger
+{
+	public partial class FormLog : Form
+	{
+		// timer to control when we take samples from the DUT
+		private System.Timers.Timer _timer;
+
+		// CSV file writer to log data captured from DUT
+		private CsvWriter _writer;
+
+		// number of samples logged
+		private ulong _samples;
+
+		/// <summary>
+		/// Flag to tell if we're logging or not.
+		/// </summary>
+		/// <remarks>
+		/// Has 3 states:  2 = stopped; 1 = running; 0 = idle.
+		/// Initialized to "stopped" state.
+		/// </remarks>
+		private int _logging = 2;
+
+		// This is a thread synchronizeation event handler; it makes us wait until the
+		// timer finishes its current task before closing the resources it's using.
+		EventWaitHandle _loggingLock = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public FormLog()
+		{
+			// Initialize the form.
+			InitializeComponent();
+
+			// Recall the most recently used log file.
+			textBoxFilename.Text = Properties.Settings.Default.Filename;
+
+			// Find all available serial ports.
+			foreach (string s in SerialPort.GetPortNames())
+			{
+				comboBoxSerialPort.Items.Add(s);
+			}
+
+			// Select the most recently used serial port.
+			comboBoxSerialPort.Text = Properties.Settings.Default.Port;
+
+			// Instantiate the timer and set the most recent interval.
+			_timer = new System.Timers.Timer(Properties.Settings.Default.Interval);
+
+			// Set the method which runs when the timer elapses.
+			_timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+			// We need to wait until one event is done to try another one.
+			_timer.AutoReset = false;
+		}
+
+		private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// Exit the program.
+			Application.Exit();
+		}
+
+		/// <summary>
+		/// When the "Browse" button is clicked, show a filebrowser.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ButtonBrowse_Click(object sender, EventArgs e)
+		{
+			// Create a file browser.
+			OpenFileDialog openFileDialog = new OpenFileDialog()
+			{
+				InitialDirectory = textBoxFilename.Text,
+				Title = "Browse Log Files",
+				CheckFileExists = false,
+				CheckPathExists = true,
+				DefaultExt = "csv",
+				Filter = "txt files (*.txt)|*.txt|csv files (*.csv)|*.csv|All files (*.*)|*.*",
+				FilterIndex = 2,
+			};
+
+			// Show the dialog box to the user.
+			// If the user selects a file...
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				// Use that file.
+				textBoxFilename.Text = openFileDialog.FileName;
+
+				// Remember it for next time.
+				Properties.Settings.Default.Filename = openFileDialog.FileName;
+			}
+		}
+
+		/// <summary>
+		/// Before exiting, check the user's wishes and safely end testing.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void FormLog_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			// Stop the logging operation.
+			StopLogging();
+
+			// Save settings.
+			Properties.Settings.Default.Save();
+		}
+
+		private void ButtonStart_Click(object sender, EventArgs e)
+		{
+			// Toggle the logging process (by enabling/disabling the timer which does the logging).
+			if (_logging != 2)
+			{
+				StopLogging();
+			}
+			else
+			{
+				StartLogging();
+			}
+		}
+
+		private void ButtonStop_Click(object sender, EventArgs e)
+		{
+			// TODO:  Split up the start and stop buttons.
+		}
+
+		private void ComboBoxSerialPort_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			// Save the serial port number to the application settings file.
+			Properties.Settings.Default.Port = comboBoxSerialPort.Text;
+		}
+
+		private void NumericUpDownInterval_ValueChanged(object sender, EventArgs e)
+		{
+			// Set the timer's interval property (which controls how often samples are taken).
+			_timer.Interval = decimal.ToDouble(numericUpDownInterval.Value);
+		}
+
+		private void OnTimedEvent(object source, ElapsedEventArgs e)
+		{
+			// If logging = 0 "idle", set logging = 1 "running"
+			// but don't let another thread change logging at the same time.
+			// If logging was not 0 "idle", don't do anything.
+			if (Interlocked.CompareExchange(ref _logging, 1, 0) != 0)
+			{
+				return;
+			}
+
+			try
+			{
+				// TODO:  Fetch a value from the DUT.
+				string sample = string.Empty;
+				int status = 0;
+				// status = _dut.GetSample(out sample);
+
+				// If we can't talk to the DUT, notify the user (but leave the timer on).
+				if (status == 1)
+				{
+					MessageBox.Show("Communication timeout.  No sample to log :(", "ERROR");
+				}
+				else
+				{
+					// Display the sample value to the user.
+					// Use a method invoker because it needs to happen in the Form's thread.
+					// It must be asynchronous (BeginInvoke, not Invoke) or else the two
+					// threads might get stuck waiting for each other).
+					BeginInvoke(new Action(() => textBoxResponse.Text = sample));
+
+					// Log a timestamp and the sample value to a CSV file.
+					List<string> row = new List<string>();
+					row.Add(e.SignalTime.ToString());
+					row.Add(sample);
+					_writer.WriteRow(row);
+
+					// Update the status message.
+					_samples++;
+					toolStripStatusLabel1.Text = "Logged " + _samples + " samples.";
+				}
+
+				// Start the timer again.
+				_timer.Enabled = true;
+			}
+			// If an error occurs...
+			catch (Exception ex)
+			{
+				// Notify the user.
+				MessageBox.Show(ex.Message, "ERROR");
+
+				// Stop logging.
+				// Use a method invoker because it needs to happen in the form's thread.
+				BeginInvoke(new Action(StopLogging));
+			}
+
+			// Set timer to "Idle" state and broadcast that we've finished executing.
+			_logging = 0;
+			_loggingLock.Set();
+		}
+
+		private void StopLogging()
+		{
+			// Don't do anything if logging is already in "stopped" state.
+			// Otherwise it will get stuck in the WaitOne loop.
+			if (_logging != 2)
+			{
+				// Ensure timer is in "idle" state before stopping it.
+				// If logging = 0 "idle", set logging = 2 "stopped"
+				// but don't let another thrad change logging at the same time.
+				// If logging was not 0 "idle", wait until it is.
+				while (Interlocked.CompareExchange(ref _logging, 2, 0) != 0)
+				{
+					_loggingLock.WaitOne();
+				}
+
+				try
+				{
+					// Disable the timer.
+					_timer.Enabled = false;
+
+					// Close the filestream if necessary.
+					_writer?.Close();
+
+					// TODO:  Close the DUT serial port.
+
+					// Update GUI.
+					toolStripStatusLabel1.Text = "Ready...";
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message, "ERROR");
+				}
+			}
+		}
+
+		private void StartLogging()
+		{
+			try
+			{
+				// Update GUI.
+				toolStripStatusLabel1.Text = "Logging data...";
+
+				// TODO:  Open the DUT serial port if necessary.
+
+				// Set up the CSV file writer filestream.
+				_writer = new CsvWriter(Properties.Settings.Default.Filename, true);
+
+				// Start the timer.
+				_timer.Enabled = true;
+
+				// Set logging state to 0 "idle".
+			}
+			// If an error occurs...
+			catch (Exception ex)
+			{
+				// Notify the user.
+				MessageBox.Show(ex.Message, "ERROR");
+
+				// Undo whatever was started.
+				StopLogging();
+			}
+		}
+	}
+}
