a65831a2d481a38795961f7566939df2ee4f6c67
Adam Johnson <adamj537@gmail.com>
2021-04-28 14:40:49 -0500

    Logfile will contain columns for all setpoints and readings for all devices used in a test.

--- Sensit.App.Calibration/Log.cs
+++ Sensit.App.Calibration/Log.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using Sensit.TestSDK.Files;
+using Sensit.TestSDK.Interfaces;
 
 namespace Sensit.App.Calibration
 {
@@ -16,14 +17,19 @@ namespace Sensit.App.Calibration
 		// name of the CSV file
 		private string _filename;
 
+		private Equipment _equipment;
+
 		/// <summary>
 		/// Constructor
 		/// </summary>
 		/// <param name="filename">desired filename to create/append to</param>
-		public Log(string filename)
+		public Log(string filename, Equipment equipment)
 		{
 			// Remember the file name.
 			_filename = filename;
+
+			// Save the equipment object (and check for null).
+			_equipment = equipment ?? throw new ArgumentNullException(nameof(equipment));
 		}
 
 		/// <summary>
@@ -34,6 +40,27 @@ namespace Sensit.App.Calibration
 		{
 			// Set up the CSV file writer filestream.
 			_writer = new CsvWriter(_filename, true);
+
+			// Write column headers.
+			List<string> row = new List<string>();
+
+			// Write column header for elapsed time.
+			row.Add("Elapsed Time");
+			
+			foreach (KeyValuePair<string, IDevice> device in _equipment.Devices)
+			{
+				// Add column headers for each setpoint.
+				foreach (KeyValuePair<VariableType, double> setpoint in device.Value.Setpoints)
+				{
+					row.Add(device.Key + " " + setpoint.Key);
+				}
+
+				// Add column headers for each reading.
+				foreach (KeyValuePair<VariableType, double> reading in device.Value.Readings)
+				{
+					row.Add(device.Key + " " + reading.Key);
+				}
+			}
 		}
 
 		/// <summary>
@@ -42,14 +69,28 @@ namespace Sensit.App.Calibration
 		/// <param name="elapsedTime"></param>
 		/// <param name="setpoint"></param>
 		/// <param name="reference"></param>
-		public void Write(TimeSpan elapsedTime, double setpoint, double reference)
+		public void Write(TimeSpan elapsedTime)
 		{
 			// Save test results to csv file.
 			// Log a timestamp and the sample value to a CSV file.
 			List<string> row = new List<string>();
 			row.Add(elapsedTime.ToString());
-			row.Add(setpoint.ToString());
-			row.Add(reference.ToString());
+
+			foreach (KeyValuePair<string, IDevice> device in _equipment.Devices)
+			{
+				// Add column headers for each setpoint.
+				foreach (KeyValuePair<VariableType, double> setpoint in device.Value.Setpoints)
+				{
+					row.Add(setpoint.Value.ToString());
+				}
+
+				// Add column headers for each reading.
+				foreach (KeyValuePair<VariableType, double> reading in device.Value.Readings)
+				{
+					row.Add(reading.Value.ToString());
+				}
+			}
+
 			_writer.WriteRow(row);
 		}
 
