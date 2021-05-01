119b110ecfc09c278e72b4af05f2e3ebb890950b
Adam Johnson <adamj537@gmail.com>
2021-05-01 08:42:13 -0500

    Added properties and improved layout.

--- Sensit.App.Calibration/UserControlVariableStatus.Designer.cs
+++ Sensit.App.Calibration/UserControlVariableStatus.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace Sensit.App.Calibration
+namespace Sensit.TestSDK.Controls
 {
 	partial class UserControlVariableStatus
 	{
@@ -29,75 +29,108 @@ namespace Sensit.App.Calibration
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.groupBoxMassFlow = new System.Windows.Forms.GroupBox();
-			this.labelVar1Unit2 = new System.Windows.Forms.Label();
-			this.labelVar1Unit1 = new System.Windows.Forms.Label();
-			this.textBoxMassFlowValue = new System.Windows.Forms.TextBox();
-			this.textBoxMassFlowSetpoint = new System.Windows.Forms.TextBox();
-			this.groupBoxMassFlow.SuspendLayout();
+			this.groupBox = new System.Windows.Forms.GroupBox();
+			this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.textBoxValue = new System.Windows.Forms.TextBox();
+			this.textBoxSetpoint = new System.Windows.Forms.TextBox();
+			this.labelPlusMinus = new System.Windows.Forms.Label();
+			this.labelUnitOfMeasure = new System.Windows.Forms.Label();
+			this.textBoxTolerance = new System.Windows.Forms.TextBox();
+			this.labelDesired = new System.Windows.Forms.Label();
+			this.groupBox.SuspendLayout();
+			this.tableLayoutPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// groupBoxMassFlow
+			// groupBox
 			// 
-			this.groupBoxMassFlow.AutoSize = true;
-			this.groupBoxMassFlow.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.groupBoxMassFlow.Controls.Add(this.labelVar1Unit2);
-			this.groupBoxMassFlow.Controls.Add(this.labelVar1Unit1);
-			this.groupBoxMassFlow.Controls.Add(this.textBoxMassFlowValue);
-			this.groupBoxMassFlow.Controls.Add(this.textBoxMassFlowSetpoint);
-			this.groupBoxMassFlow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.groupBoxMassFlow.Location = new System.Drawing.Point(4, 4);
-			this.groupBoxMassFlow.Margin = new System.Windows.Forms.Padding(4);
-			this.groupBoxMassFlow.Name = "groupBoxMassFlow";
-			this.groupBoxMassFlow.Padding = new System.Windows.Forms.Padding(4);
-			this.groupBoxMassFlow.Size = new System.Drawing.Size(175, 124);
-			this.groupBoxMassFlow.TabIndex = 1;
-			this.groupBoxMassFlow.TabStop = false;
-			this.groupBoxMassFlow.Text = "Mass Flow";
+			this.groupBox.AutoSize = true;
+			this.groupBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.groupBox.Controls.Add(this.tableLayoutPanel);
+			this.groupBox.Location = new System.Drawing.Point(3, 3);
+			this.groupBox.Name = "groupBox";
+			this.groupBox.Size = new System.Drawing.Size(452, 49);
+			this.groupBox.TabIndex = 0;
+			this.groupBox.TabStop = false;
+			this.groupBox.Text = "Title";
 			// 
-			// labelVar1Unit2
+			// tableLayoutPanel
 			// 
-			this.labelVar1Unit2.AutoSize = true;
-			this.labelVar1Unit2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.labelVar1Unit2.Location = new System.Drawing.Point(109, 66);
-			this.labelVar1Unit2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-			this.labelVar1Unit2.Name = "labelVar1Unit2";
-			this.labelVar1Unit2.Size = new System.Drawing.Size(58, 25);
-			this.labelVar1Unit2.TabIndex = 5;
-			this.labelVar1Unit2.Text = "sccm";
+			this.tableLayoutPanel.AutoSize = true;
+			this.tableLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.tableLayoutPanel.ColumnCount = 6;
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel.Controls.Add(this.textBoxValue, 0, 0);
+			this.tableLayoutPanel.Controls.Add(this.textBoxSetpoint, 2, 0);
+			this.tableLayoutPanel.Controls.Add(this.labelPlusMinus, 3, 0);
+			this.tableLayoutPanel.Controls.Add(this.labelUnitOfMeasure, 5, 0);
+			this.tableLayoutPanel.Controls.Add(this.textBoxTolerance, 4, 0);
+			this.tableLayoutPanel.Controls.Add(this.labelDesired, 1, 0);
+			this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel.Location = new System.Drawing.Point(3, 18);
+			this.tableLayoutPanel.Name = "tableLayoutPanel";
+			this.tableLayoutPanel.RowCount = 1;
+			this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel.Size = new System.Drawing.Size(446, 28);
+			this.tableLayoutPanel.TabIndex = 0;
 			// 
-			// labelVar1Unit1
+			// textBoxValue
 			// 
-			this.labelVar1Unit1.AutoSize = true;
-			this.labelVar1Unit1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.labelVar1Unit1.Location = new System.Drawing.Point(109, 27);
-			this.labelVar1Unit1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-			this.labelVar1Unit1.Name = "labelVar1Unit1";
-			this.labelVar1Unit1.Size = new System.Drawing.Size(58, 25);
-			this.labelVar1Unit1.TabIndex = 4;
-			this.labelVar1Unit1.Text = "sccm";
+			this.textBoxValue.Location = new System.Drawing.Point(3, 3);
+			this.textBoxValue.Name = "textBoxValue";
+			this.textBoxValue.ReadOnly = true;
+			this.textBoxValue.Size = new System.Drawing.Size(100, 22);
+			this.textBoxValue.TabIndex = 2;
 			// 
-			// textBoxMassFlowValue
+			// textBoxSetpoint
 			// 
-			this.textBoxMassFlowValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.textBoxMassFlowValue.Location = new System.Drawing.Point(8, 63);
-			this.textBoxMassFlowValue.Margin = new System.Windows.Forms.Padding(4);
-			this.textBoxMassFlowValue.Name = "textBoxMassFlowValue";
-			this.textBoxMassFlowValue.ReadOnly = true;
-			this.textBoxMassFlowValue.Size = new System.Drawing.Size(92, 30);
-			this.textBoxMassFlowValue.TabIndex = 1;
-			this.textBoxMassFlowValue.Text = "0.000";
+			this.textBoxSetpoint.Location = new System.Drawing.Point(176, 3);
+			this.textBoxSetpoint.Name = "textBoxSetpoint";
+			this.textBoxSetpoint.ReadOnly = true;
+			this.textBoxSetpoint.Size = new System.Drawing.Size(100, 22);
+			this.textBoxSetpoint.TabIndex = 3;
 			// 
-			// textBoxMassFlowSetpoint
+			// labelPlusMinus
 			// 
-			this.textBoxMassFlowSetpoint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.textBoxMassFlowSetpoint.Location = new System.Drawing.Point(8, 23);
-			this.textBoxMassFlowSetpoint.Margin = new System.Windows.Forms.Padding(4);
-			this.textBoxMassFlowSetpoint.Name = "textBoxMassFlowSetpoint";
-			this.textBoxMassFlowSetpoint.ReadOnly = true;
-			this.textBoxMassFlowSetpoint.Size = new System.Drawing.Size(92, 30);
-			this.textBoxMassFlowSetpoint.TabIndex = 0;
-			this.textBoxMassFlowSetpoint.Text = "0.000";
+			this.labelPlusMinus.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.labelPlusMinus.AutoSize = true;
+			this.labelPlusMinus.Location = new System.Drawing.Point(282, 5);
+			this.labelPlusMinus.Name = "labelPlusMinus";
+			this.labelPlusMinus.Size = new System.Drawing.Size(16, 17);
+			this.labelPlusMinus.TabIndex = 4;
+			this.labelPlusMinus.Text = "±";
+			// 
+			// labelUnitOfMeasure
+			// 
+			this.labelUnitOfMeasure.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.labelUnitOfMeasure.AutoSize = true;
+			this.labelUnitOfMeasure.Location = new System.Drawing.Point(410, 5);
+			this.labelUnitOfMeasure.Name = "labelUnitOfMeasure";
+			this.labelUnitOfMeasure.Size = new System.Drawing.Size(33, 17);
+			this.labelUnitOfMeasure.TabIndex = 5;
+			this.labelUnitOfMeasure.Text = "Unit";
+			// 
+			// textBoxTolerance
+			// 
+			this.textBoxTolerance.Location = new System.Drawing.Point(304, 3);
+			this.textBoxTolerance.Name = "textBoxTolerance";
+			this.textBoxTolerance.ReadOnly = true;
+			this.textBoxTolerance.Size = new System.Drawing.Size(100, 22);
+			this.textBoxTolerance.TabIndex = 6;
+			// 
+			// labelDesired
+			// 
+			this.labelDesired.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.labelDesired.AutoSize = true;
+			this.labelDesired.Location = new System.Drawing.Point(109, 5);
+			this.labelDesired.Name = "labelDesired";
+			this.labelDesired.Size = new System.Drawing.Size(61, 17);
+			this.labelDesired.TabIndex = 7;
+			this.labelDesired.Text = "Desired:";
 			// 
 			// UserControlVariableStatus
 			// 
@@ -105,11 +138,13 @@ namespace Sensit.App.Calibration
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoSize = true;
 			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.Controls.Add(this.groupBoxMassFlow);
+			this.Controls.Add(this.groupBox);
 			this.Name = "UserControlVariableStatus";
-			this.Size = new System.Drawing.Size(183, 132);
-			this.groupBoxMassFlow.ResumeLayout(false);
-			this.groupBoxMassFlow.PerformLayout();
+			this.Size = new System.Drawing.Size(458, 55);
+			this.groupBox.ResumeLayout(false);
+			this.groupBox.PerformLayout();
+			this.tableLayoutPanel.ResumeLayout(false);
+			this.tableLayoutPanel.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -117,10 +152,13 @@ namespace Sensit.App.Calibration
 
 		#endregion
 
-		private System.Windows.Forms.GroupBox groupBoxMassFlow;
-		private System.Windows.Forms.Label labelVar1Unit2;
-		private System.Windows.Forms.Label labelVar1Unit1;
-		private System.Windows.Forms.TextBox textBoxMassFlowValue;
-		private System.Windows.Forms.TextBox textBoxMassFlowSetpoint;
+		private System.Windows.Forms.GroupBox groupBox;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+		private System.Windows.Forms.TextBox textBoxValue;
+		private System.Windows.Forms.TextBox textBoxSetpoint;
+		private System.Windows.Forms.Label labelPlusMinus;
+		private System.Windows.Forms.Label labelUnitOfMeasure;
+		private System.Windows.Forms.TextBox textBoxTolerance;
+		private System.Windows.Forms.Label labelDesired;
 	}
 }
--- Sensit.App.Calibration/UserControlVariableStatus.cs
+++ Sensit.App.Calibration/UserControlVariableStatus.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows.Forms;
 
-namespace Sensit.App.Calibration
+namespace Sensit.TestSDK.Controls
 {
 	public partial class UserControlVariableStatus : UserControl
 	{
@@ -16,5 +10,39 @@ namespace Sensit.App.Calibration
 		{
 			InitializeComponent();
 		}
+
+		#region Properties
+
+		public string Title
+		{
+			get => groupBox.Text;
+			set => groupBox.Text = value;
+		}
+
+		public decimal Value
+		{
+			get => Convert.ToDecimal(textBoxValue.Text, CultureInfo.InvariantCulture);
+			set => textBoxValue.Text = value.ToString(CultureInfo.CurrentCulture);
+		}
+
+		public decimal Setpoint
+		{
+			get => Convert.ToDecimal(textBoxSetpoint.Text, CultureInfo.InvariantCulture);
+			set => textBoxSetpoint.Text = value.ToString(CultureInfo.CurrentCulture);
+		}
+
+		public decimal Tolerance
+		{
+			get => Convert.ToDecimal(textBoxTolerance.Text, CultureInfo.InvariantCulture);
+			set => textBoxTolerance.Text = value.ToString(CultureInfo.CurrentCulture);
+		}
+
+		public string UnitOfMeasure
+		{
+			get => labelUnitOfMeasure.Text;
+			set => labelUnitOfMeasure.Text = value;
+		}
+
+		#endregion
 	}
 }
