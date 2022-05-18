5fad0b8d5d6ce0695d39723e5b6491c44102fe66
Adam Johnson <adamj537@gmail.com>
2022-05-18 16:03:05 -0500

    Improved form layout.

--- Sensit.TestSDK/Forms/FormInputDialog.Designer.cs
+++ Sensit.TestSDK/Forms/FormInputDialog.Designer.cs
@@ -30,9 +30,9 @@
 		{
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-			this.label1 = new System.Windows.Forms.Label();
 			this.buttonOk = new System.Windows.Forms.Button();
 			this.buttonCancel = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
 			this.tableLayoutPanel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
 			this.SuspendLayout();
@@ -42,47 +42,41 @@
 			this.tableLayoutPanel1.AutoSize = true;
 			this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.tableLayoutPanel1.ColumnCount = 2;
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
 			this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 0, 1);
 			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.buttonOk, 0, 3);
 			this.tableLayoutPanel1.Controls.Add(this.buttonCancel, 1, 3);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 4;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 15F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(162, 78);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(232, 128);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// numericUpDown1
 			// 
 			this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.None;
 			this.tableLayoutPanel1.SetColumnSpan(this.numericUpDown1, 2);
-			this.numericUpDown1.Location = new System.Drawing.Point(21, 16);
+			this.numericUpDown1.Location = new System.Drawing.Point(36, 25);
+			this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.numericUpDown1.Name = "numericUpDown1";
-			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.Size = new System.Drawing.Size(160, 27);
 			this.numericUpDown1.TabIndex = 2;
-			// 
-			// label1
-			// 
-			this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.label1.AutoSize = true;
-			this.tableLayoutPanel1.SetColumnSpan(this.label1, 2);
-			this.label1.Location = new System.Drawing.Point(81, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(0, 13);
-			this.label1.TabIndex = 3;
 			// 
 			// buttonOk
 			// 
 			this.buttonOk.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.buttonOk.Location = new System.Drawing.Point(3, 52);
+			this.buttonOk.Location = new System.Drawing.Point(8, 82);
+			this.buttonOk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.buttonOk.Name = "buttonOk";
-			this.buttonOk.Size = new System.Drawing.Size(75, 23);
+			this.buttonOk.Size = new System.Drawing.Size(100, 35);
 			this.buttonOk.TabIndex = 0;
 			this.buttonOk.Text = "&OK";
 			this.buttonOk.UseVisualStyleBackColor = true;
@@ -91,23 +85,36 @@
 			// 
 			this.buttonCancel.Anchor = System.Windows.Forms.AnchorStyles.None;
 			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonCancel.Location = new System.Drawing.Point(84, 52);
+			this.buttonCancel.Location = new System.Drawing.Point(124, 82);
+			this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.buttonCancel.Name = "buttonCancel";
-			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+			this.buttonCancel.Size = new System.Drawing.Size(100, 35);
 			this.buttonCancel.TabIndex = 1;
 			this.buttonCancel.Text = "&Cancel";
 			this.buttonCancel.UseVisualStyleBackColor = true;
 			// 
+			// label1
+			// 
+			this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label1.AutoSize = true;
+			this.tableLayoutPanel1.SetColumnSpan(this.label1, 2);
+			this.label1.Location = new System.Drawing.Point(116, 0);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 20);
+			this.label1.TabIndex = 3;
+			// 
 			// FormInputDialog
 			// 
 			this.AcceptButton = this.buttonOk;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.AutoSize = true;
 			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 			this.CancelButton = this.buttonCancel;
-			this.ClientSize = new System.Drawing.Size(186, 95);
+			this.ClientSize = new System.Drawing.Size(232, 128);
 			this.Controls.Add(this.tableLayoutPanel1);
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "FormInputDialog";
